300446562eb5f40236062febde133727c8373efa
ozco <i2am0learning2python1@gmail.com>
2024-03-30 22:24:11 +0200

    preparation of functions

---
+++ program_ex2_2.py
@@ -0,0 +1,13 @@
+
+
+def bus_routes():
+    pass
+
+def how_many_people_rode_bus(routes, date):
+    pass
+
+def total_number_of_rides():
+    pass
+
+def increase_in_the_number_of_passengers():
+    pass
