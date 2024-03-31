189182c82af257d5857f3a2fdef348206fed54ff
ozco <i2am0learning2python1@gmail.com>
2024-03-31 14:58:03 +0300

    number of bus routes

--- program_ex2_2.py
+++ program_ex2_2.py
@@ -1,7 +1,13 @@
+from readrides import read_rides_as_dictionary
 
+rows = read_rides_as_dictionary('python-mastery-main\\Data\\ctabus.csv')
 
-def bus_routes():
-    pass
+def bus_routes(lst_rows: list):
+    lst_route = []
+    for i in lst_rows:
+        lst_route.append(i['route'])
+    return len(set(lst_route))
+    
 
 def how_many_people_rode_bus(routes, date):
     pass
@@ -11,3 +17,6 @@ def total_number_of_rides():
 
 def increase_in_the_number_of_passengers():
     pass
+
+if __name__ == '__main__':
+    print(f'There are {bus_routes(rows)} bus routes in Chicago')
