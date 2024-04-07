3947b91c0c14d377fd3d6208426a120120c5d682
ozco <i2am0learning2python1@gmail.com>
2024-04-07 21:45:21 +0300

    Read a CSV file with column type conversion

---
+++ reader.py
@@ -0,0 +1,17 @@
+# reader.py
+
+import csv
+#from collections import defaultdict
+
+def read_csv_as_dicts(filename: str, types: list):
+    '''
+    Read a CSV file with column type conversion
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            record = {name: func(val) for name, func, val in zip(headers, types, row)}
+            records.append(record)
+    return records
