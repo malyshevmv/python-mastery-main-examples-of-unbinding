7a385aeb9491ba97f6fb7af90b5a3c7f212991c0
ozco <i2am0learning2python1@gmail.com>
2024-04-07 21:56:21 +0300

    Modified, exercise 3.1

--- stock.py
+++ stock.py
@@ -1,3 +1,6 @@
+from os import read
+import csv
+
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
@@ -5,3 +8,13 @@ class Stock:
         self.price = price
     def cost(self):
         return self.shares * self.price
+
+def read_portfolio(filename:str):
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for name, shares, price in rows:
+            record = Stock(name, shares, price)
+            records.append(record)
+    return records
