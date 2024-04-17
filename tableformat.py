4353962532f44e9fb4d1d8721bc483176ee36a10
ozco <i2am0learning2python1@gmail.com>
2024-04-17 21:08:28 +0300

    ex3_8

--- tableformat.py
+++ tableformat.py
@@ -1,8 +1,11 @@
+from abc import ABC, abstractmethod
 
-class TableFormatter:
+class TableFormatter(ABC):
+    @abstractmethod
     def headings(self, headers):
         raise NotImplementedError()
 
+    @abstractmethod
     def row(self, rowdata):
         raise NotImplementedError()
 
@@ -34,7 +37,17 @@ class HTMLTableFormatter(TableFormatter):
             print(f'<tr>{i}</tr>', end=' ')
         print('</tr>')
 
-def create_formatter(name: str):
+class ColumnFormatMixin:
+    formats = []
+    def row(self, rowdata):
+        rowdata = [(fmt % d) for fmt, d in zip(self.formats, rowdata)]
+        super().row(rowdata)
+
+class UpperHeadersMixin:
+    def headings(self, headers):
+        super().headings([h.upper() for h in headers])
+
+def create_formatter(name: str, column_formats=None, upper_headers=True):
     if name == 'text':
         formatter_cls = TextTableFormatter
     elif name == 'csv':
@@ -43,11 +56,20 @@ def create_formatter(name: str):
         formatter_cls = HTMLTableFormatter
     else:
         raise RuntimeError(f'unknow format {name}')
+    if column_formats:
+        class formatter_cls(ColumnFormatMixin, formatter_cls):
+              formats = column_formats
+
+    if upper_headers:
+        class formatter_cls(UpperHeadersMixin, formatter_cls):
+            pass
     return formatter_cls()
         
 
 def print_table(lst:list, lst_name:list, formatter):
     '''printing the table according to the given list(lst_name) '''
+    if not isinstance(formatter, TableFormatter):
+        raise TypeError('Expected a TableFormatter')
     formatter.headings(lst_name)
     #for i in lst_name:
     #    print(i.rjust(10), end=' ')
