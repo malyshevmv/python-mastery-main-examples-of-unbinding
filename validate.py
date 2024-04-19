c6f88a0bab7a9f03ea1e997e57a2b016a15f1041
ozco <i2am0learning2python1@gmail.com>
2024-04-19 14:54:52 +0300

    ex4_4

--- validate.py
+++ validate.py
@@ -1,8 +1,16 @@
 # validate.py
 class Validator:
+    def __init__(self, name=None):
+        self.name = name
+
+    def __set_name__(self, cls, name):
+        self.name = name
+
     @classmethod
     def check(cls, value):
         return value
+    def __set__(self, instance,	value):
+        instance.__dict__[self.name] = self.check(value)
 
 class Typed(Validator):
     expected_type = object
@@ -47,6 +55,9 @@ class NonEmptyString(String, NonEmpty):
 if __name__ == '__main__':
     class Stock:
         __slots__ = ('name', '_shares', '_price')
+        name = NonEmptyString()
+        shares = PositiveInteger()
+        price = PositiveFloat()
         def __init__(self, name, shares, price):
             self.name = name
             self.shares = shares
